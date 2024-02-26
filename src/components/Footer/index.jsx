745a5f3bd0ac20f7ff90e67c12da2db5c7b831e1
MHerholdt94 <mathiasbherholdt@gmail.com>
2024-02-26 17:25:35 +0100

    adds basic styling to header and footer components

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import styled from "styled-components";
+
+const FooterElement = styled.footer`
+	padding: 1rem;
+	z-index: 100;
+	color: #fff;
+	background-color: #252525;
+	box-shadow: 0 0 10px 0 #252525;
+	flex-shrink: 0;
+`;
 
 function Footer() {
 	return (
-		<footer>
+		<FooterElement>
 			<div>©2024 Allmart</div>
-		</footer>
+		</FooterElement>
 	);
 }
 
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,18 +1,53 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const HeaderElement = styled.header`
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	padding: 1rem;
+	background-color: #252525;
+	color: #fff;
+	box-shadow: 0 0 10px 0 #252525;
+	z-index: 100;
+
+	ul {
+		display: flex;
+		justify-content: center;
+		margin: 0;
+		padding: 0;
+		list-style: none;
+		gap: 1rem;
+	}
+
+	li a {
+		text-decoration: none;
+		color: #fff;
+	}
+`;
 
 function Header() {
 	return (
-		<header>
+		<HeaderElement>
 			<div>ALLMART</div>
 			<nav>
 				<ul>
-					<li>Home</li>
-					<li>Products</li>
-					<li>Cart</li>
-					<li>Contact</li>
+					<li>
+						<Link to="/">Home</Link>
+					</li>
+					<li>
+						<Link to="/products">Products</Link>
+					</li>
+					<li>
+						<Link to="/cart">Cart</Link>
+					</li>
+					<li>
+						<Link to="/contact">Contact</Link>
+					</li>
 				</ul>
 			</nav>
-		</header>
+		</HeaderElement>
 	);
 }
 
