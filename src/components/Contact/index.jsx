bf93838bdd8924140efa5f689acc5d1e244cfb75
MHerholdt94 <mathiasbherholdt@gmail.com>
2024-03-01 12:43:55 +0100

    enhances contact form styles

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -8,7 +8,9 @@ function Contact() {
 		handleSubmit,
 		formState: { errors },
 	} = useForm();
+
 	const onSubmit = (data) => console.log(data);
+
 	console.log(errors);
 
 	return (
@@ -16,28 +18,46 @@ function Contact() {
 			<h1>Contact</h1>
 
 			<S.ContactContainer>
+				<S.ContactTitle>
+					<h2>Get in touch</h2>
+					<p>Send us a question or inquiry</p>
+				</S.ContactTitle>
 				<S.ContactForm onSubmit={handleSubmit(onSubmit)}>
-					<input
+					<label htmlFor="fullName">Full Name</label>
+					<S.FormInput
+						id="fullName"
 						type="text"
 						placeholder="Full name"
-						{...register("Full name", { required: true, min: 3 })}
+						{...register("fullName", { required: true, min: 3 })}
 					/>
-					<input
+
+					<label htmlFor="subject">Subject</label>
+					<S.FormInput
+						id="subject"
 						type="text"
 						placeholder="Subject"
-						{...register("Subject", { required: true })}
+						{...register("subject", { required: true })}
 					/>
-					<input
+
+					<label htmlFor="email">Email</label>
+					<S.FormInput
+						id="email"
 						type="text"
 						placeholder="Email"
-						{...register("Email", {
+						{...register("email", {
 							required: true,
 							pattern: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/i,
 						})}
 					/>
-					<textarea {...register("Body", { required: true, min: 3 })} />
 
-					<input type="submit" />
+					<label htmlFor="message">Message</label>
+					<S.FormTextarea
+						id="message"
+						placeholder="Message"
+						{...register("message", { required: true, min: 3 })}
+					/>
+
+					<S.FormSubmit type="submit" />
 				</S.ContactForm>
 			</S.ContactContainer>
 		</main>
--- src/components/Contact/styles.js
+++ src/components/Contact/styles.js
@@ -3,47 +3,73 @@ import styled from "styled-components";
 export const ContactContainer = styled.div`
 	display: flex;
 	flex-direction: column;
-	justify-content: center;
-	align-items: center;
+	margin: auto;
 	padding: 1em;
 	background-color: ${(props) => props.theme.colors.black};
 	box-shadow: 1px 1px 5px rgba(255, 255, 255, 0.1);
 	border-radius: 10px;
-	width: 80%;
-	margin: auto;
+	max-width: 400px;
+`;
+
+export const ContactTitle = styled.div`
+	display: flex;
+	flex-direction: column;
+	border-bottom: 1px solid ${(props) => props.theme.colors.white};
+	padding: 0.5em 0.5em 1em;
+	margin-top: 0;
+	margin-bottom: 1em;
+
+	h2,
+	p {
+		margin: 0;
+	}
+
+	p {
+		color: ${(props) => props.theme.colors.whiteAlt};
+	}
 `;
 
 export const ContactForm = styled.form`
 	display: flex;
 	flex-direction: column;
-	width: 100%;
+	padding: 0 0.5em 0.5em;
+	max-width: 100%;
 
-	input,
-	textarea {
-		margin-bottom: 1em;
-		padding: 0.5em;
-		font-size: 1em;
-		border: 1px solid #ddd;
-		border-radius: 5px;
+	label {
+		margin-bottom: 4px;
 	}
+`;
 
-	textarea {
-		height: 150px;
-		resize: none;
-	}
+export const FormInput = styled.input`
+	margin-bottom: 1em;
+	padding: 0.5em;
+	font-size: 1em;
+	border: 1px solid #ddd;
+	border-radius: 5px;
+`;
 
-	input[type="submit"] {
-		background-color: ${(props) => props.theme.colors.primary};
-		color: ${(props) => props.theme.colors.black};
-		cursor: pointer;
-		border: none;
-		border-radius: 5px;
-		padding: 0.5em;
-		font-size: 1em;
-		transition: background-color 0.3s ease;
+export const FormTextarea = styled.textarea`
+	font-family: inherit;
+	margin-bottom: 1em;
+	padding: 0.5em;
+	font-size: 1em;
+	border: 1px solid #ddd;
+	border-radius: 5px;
+	height: 150px;
+	resize: none;
+`;
 
-		&:hover {
-			background-color: ${(props) => props.theme.colors.primaryAlt};
-		}
+export const FormSubmit = styled.input`
+	background-color: ${(props) => props.theme.colors.primary};
+	color: ${(props) => props.theme.colors.black};
+	cursor: pointer;
+	border: none;
+	border-radius: 5px;
+	padding: 0.5em;
+	font-size: 1em;
+	transition: background-color 0.3s ease;
+
+	&:hover {
+		background-color: ${(props) => props.theme.colors.primaryAlt};
 	}
 `;
