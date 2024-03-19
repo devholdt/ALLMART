49a9e993cbadf2b4d9237be266de400d1ee5609c
MHerholdt94 <mathiasbherholdt@gmail.com>
2024-03-19 13:21:43 +0100

    chore: commit save point

--- src/components/Error/styles.js
+++ src/components/Error/styles.js
@@ -7,6 +7,33 @@ export const Container = styled.div`
 	margin: auto;
 	box-shadow: ${(props) => props.theme.boxShadows.white};
 	max-width: 520px;
+`;
+
+export const ErrorMessage = styled.div`
+	width: 100%;
+	background-color: ${(props) => props.theme.colors.red};
+	color: ${(props) => props.theme.colors.black};
+	display: flex;
+	flex-wrap: wrap;
+
+	p {
+		display: flex;
+		align-items: center;
+		gap: 0.5em;
+		padding: 0 1em;
+		font-size: ${(props) => props.theme.fontSizes.large};
+	}
+`;
+
+export const ErrorContent = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	padding: 2em;
+	color: ${(props) => props.theme.colors.black};
+	background-color: ${(props) => props.theme.colors.white};
+	text-align: center;
 
 	.errorButtons {
 		display: flex;
@@ -40,33 +67,6 @@ export const Container = styled.div`
 			color: ${(props) => props.theme.colors.blackAlt};
 		}
 	}
-`;
-
-export const ErrorMessage = styled.div`
-	width: 100%;
-	background-color: ${(props) => props.theme.colors.red};
-	color: ${(props) => props.theme.colors.black};
-	display: flex;
-	flex-wrap: wrap;
-
-	p {
-		display: flex;
-		align-items: center;
-		gap: 0.5em;
-		padding: 0 1em;
-		font-size: ${(props) => props.theme.fontSizes.large};
-	}
-`;
-
-export const ErrorContent = styled.div`
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
-	padding: 2em;
-	color: ${(props) => props.theme.colors.black};
-	background-color: ${(props) => props.theme.colors.white};
-	text-align: center;
 
 	span {
 		margin-top: 0.5em;
